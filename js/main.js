8205685b7fb54334059c090d0c346fcc6b5206df
Anna <gstudentseo@gmail.com>
2022-07-01 16:13:58 +0200

    Described the input data

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ let getSomeNumber = (min, max) => {
 
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(getSomeNumber(0.3, 10.7));
-
+//console.log(getSomeNumber(0.3, 10.7));
 
 
 let getRandomInteger = (min, max, countAfterPoint) => {
@@ -20,4 +19,26 @@ let getRandomInteger = (min, max, countAfterPoint) => {
   let number = Math.random() * (max - min) + min;
   return number.toFixed(countAfterPoint);
 }
-console.log(getRandomInteger(4.3, 15.2, 1));
+//console.log(getRandomInteger(4.3, 15.2, 1));
+
+const Author = {
+  avatar: 'img/avatars/user' + getSomeNumber(1, 10) + '.png',
+};
+
+const Offer = {};
+const Title = 'The best offer!';
+const Adress;
+const price = getSomeNumber(5, 40);
+const TYPES = [palace, flat, house, bungalow];
+const Rooms = getSomeNumber(1, 100);
+const Guests = getSomeNumber(1, 5);
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const CHECKOUT = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const Description = 'The best offer for families with children.';
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+const Location = {
+  x: getRandomInteger(35.65000, 35.70000, 5),
+  y: getRandomInteger(139.70000, 139.80000, 5),
+};
